254932852acfdc42e5ffebaee8a867c534cefc89
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2026-09-16 19:49:05 +0200

    commentaarregel twee toegevoegd

--- hoofdstuk-01-werkplek/1.16-klassikale-oefening-1.1-een-tweede-project-met-een-eigen/main.py
+++ hoofdstuk-01-werkplek/1.16-klassikale-oefening-1.1-een-tweede-project-met-een-eigen/main.py
@@ -2,6 +2,7 @@
 # Hoofdstuk 1 — Je Python-werkplek professioneel opzetten
 # Klassikale oefening 1.1 — Een tweede project met een eigen omgeving, uitwerking
 # dit is een extra regel
+# dit is een tweede regel
 
 boeken = ["De ontdekking van de hemel", "Het diner", "Turks fruit"]
 
